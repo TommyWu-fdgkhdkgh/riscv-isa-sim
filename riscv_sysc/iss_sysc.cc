145e9fee6c0a08c8ff8576115ae803e98030c92b
tommy.wu <fdgkhdkgh@gmail.com>
2023-07-22 10:18:30 +0800

    Add insn_template_sysc.cc.
    Signed-off-by: tommy.wu <fdgkhdkgh@gmail.com>

--- riscv_sysc/iss_sysc.cc
+++ riscv_sysc/iss_sysc.cc
@@ -1,12 +1,32 @@
 #include "iss_sysc.h"
 
-iss::iss()
+iss::iss(const char *isa_str, const char *priv_str)
 {
-
+  isa = new isa_parser_t(isa_str, priv_str);
 }
 
 void iss::register_insn(insn_desc_t desc)
 {
+  assert(desc.fast_rv32i && desc.fast_rv64i && desc.fast_rv32e && desc.fast_rv64e &&
+         desc.logged_rv32i && desc.logged_rv64i && desc.logged_rv32e && desc.logged_rv64e);
+
+  instructions.push_back(desc);
+}
+
+void iss::build_opcode_map()
+{
+  struct cmp {
+    bool operator()(const insn_desc_t& lhs, const insn_desc_t& rhs) {
+      if (lhs.match == rhs.match)
+        return lhs.mask > rhs.mask;
+      return lhs.match > rhs.match;
+    }
+  };
+  std::sort(instructions.begin(), instructions.end(), cmp());
+
+  for (size_t i = 0; i < OPCODE_CACHE_SIZE; i++) {
+    opcode_cache[i] = insn_desc_t::illegal();
+  }
 }
 
 void iss::register_base_instructions()
@@ -23,31 +43,37 @@ void iss::register_base_instructions()
   #undef DECLARE_OVERLAP_INSN
 
   #define DEFINE_INSN(name) \
-    extern reg_t fast_rv32i_##name(processor_t*, insn_t, reg_t); \
-    extern reg_t fast_rv64i_##name(processor_t*, insn_t, reg_t); \
-    extern reg_t fast_rv32e_##name(processor_t*, insn_t, reg_t); \
-    extern reg_t fast_rv64e_##name(processor_t*, insn_t, reg_t); \
-    extern reg_t logged_rv32i_##name(processor_t*, insn_t, reg_t); \
-    extern reg_t logged_rv64i_##name(processor_t*, insn_t, reg_t); \
-    extern reg_t logged_rv32e_##name(processor_t*, insn_t, reg_t); \
-    extern reg_t logged_rv64e_##name(processor_t*, insn_t, reg_t); \
+    extern reg_t fast_rv32i_##name_sysc(processor_t*, insn_t, reg_t); \
+    extern reg_t fast_rv64i_##name_sysc(processor_t*, insn_t, reg_t); \
+    extern reg_t fast_rv32e_##name_sysc(processor_t*, insn_t, reg_t); \
+    extern reg_t fast_rv64e_##name_sysc(processor_t*, insn_t, reg_t); \
+    extern reg_t logged_rv32i_##name_sysc(processor_t*, insn_t, reg_t); \
+    extern reg_t logged_rv64i_##name_sysc(processor_t*, insn_t, reg_t); \
+    extern reg_t logged_rv32e_##name_sysc(processor_t*, insn_t, reg_t); \
+    extern reg_t logged_rv64e_##name_sysc(processor_t*, insn_t, reg_t); \
     if (name##_supported) { \
       register_insn((insn_desc_t) { \
         name##_match, \
         name##_mask, \
-        fast_rv32i_##name, \
-        fast_rv64i_##name, \
-        fast_rv32e_##name, \
-        fast_rv64e_##name, \
-        logged_rv32i_##name, \
-        logged_rv64i_##name, \
-        logged_rv32e_##name, \
-        logged_rv64e_##name}); \
+        fast_rv32i_##name_sysc, \
+        fast_rv64i_##name_sysc, \
+        fast_rv32e_##name_sysc, \
+        fast_rv64e_##name_sysc, \
+        logged_rv32i_##name_sysc, \
+        logged_rv64i_##name_sysc, \
+        logged_rv32e_##name_sysc, \
+        logged_rv64e_##name_sysc}); \
     }
   #include "insn_list.h"
   #undef DEFINE_INSN
+
+  // terminate instruction list with a catch-all
+  register_insn(insn_desc_t::illegal());
+
+  build_opcode_map();
 }
 
 iss::~iss()
 {
+    delete isa;
 }
--- riscv_sysc/iss_sysc.h
+++ riscv_sysc/iss_sysc.h
@@ -1,14 +1,28 @@
+#ifndef _RISCV_SYSC_ISS_H
+#define _RISCV_SYSC_ISS_H
+
 #include "abstract_device.h"
 #include "processor.h"
+#include <vector>
 
 // this class represents one instruction set simulator in a RISC-V machine.
 class iss : public abstract_device_t
 {
 public:
-  iss();
+  //iss(const isa_parser_t *isa);
+  iss(const char *isa_str, const char *priv_str);
   ~iss();
 
 private:
+  // Decide this iss should support which isa-string.
+  const isa_parser_t *isa; 
+  std::vector<insn_desc_t> instructions;
+  static const size_t OPCODE_CACHE_SIZE = 8191;
+  insn_desc_t opcode_cache[OPCODE_CACHE_SIZE];
+
   void register_insn(insn_desc_t desc);
+  void build_opcode_map();
   void register_base_instructions();
 };
+
+#endif

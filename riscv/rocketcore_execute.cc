9206538d698aa972c71ecd3e25eb913547032e9e
Tommy Wu <tommy.wu@sifive.com>
2024-02-05 17:09:29 -0800

    Add a place-holder for the rocketcore micro-archi simulation.
    Signed-off-by: Tommy Wu <tommy.wu@sifive.com>

---
+++ riscv/rocketcore_execute.cc
@@ -0,0 +1,16 @@
+// See LICENSE for license details.
+
+#include "config.h"
+#include "processor.h"
+#include "mmu.h"
+#include "disasm.h"
+#include "decode_macros.h"
+#include <cassert>
+
+namespace rocketcore {
+
+void place_holder() {
+    // place_holder for rocket-core
+}
+
+};
